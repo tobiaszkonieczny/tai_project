aaae8e4b863787151bf2a1b18eaf6c685babb947
Tobiasz Konieczny <tobiasz.kon@gmail.com>
2025-12-11 23:06:28 +0100

    Added login logic, styles and xhtml pages

---
+++ src/main/java/com/tai/project_tai/controllers/ApplicationControllerXML.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tobia
+ */
+public class ApplicationControllerXML {
+       String student = "";
+    
+    public ApplicationControllerXML() {
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+}
